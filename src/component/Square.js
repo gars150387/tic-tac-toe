0746bb9ee6758d3fa863b8819bb041f0641d6bf0
Gustavo <gar.santeliz@gmail.com>
2022-01-13 20:05:35 -0500

    component created and style set

---
+++ src/component/Square.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Square = () =>{
+    return (
+        <div style={{
+            backgroundColor: 'gold',
+            margin: 10,
+            padding: 20
+        }}>
+            Square
+        </div>
+    )
+}
