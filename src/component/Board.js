c44be04e2f256946e52c0649c3beca270ec44b36
Gustavo <gar.santeliz@gmail.com>
2022-01-13 20:17:00 -0500

    columns and row added

--- src/component/Board.js
+++ src/component/Board.js
@@ -1,15 +1,30 @@
 import React from 'react'
+import './Board.css'
 import { Square } from './Square'
 
-export const Board = () =>{
+export const Board = () => {
     return (
         <div style={{
-            backgroundColor:'skyblue',
+            backgroundColor: 'skyblue',
             margin: 10,
             padding: 20
         }}>
             Board
-            <Square/>
+            <div className='board-row'>
+                <Square />
+                <Square />
+                <Square />
+            </div>
+            <div className='board-row'>
+                <Square />
+                <Square />
+                <Square />
+            </div>
+            <div className='board-row'>
+                <Square />
+                <Square />
+                <Square />
+            </div>
         </div>
     )
 }
