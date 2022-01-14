dd9f724f14d79b63bf8c723ace6bcb4792f00059
Gustavo <gar.santeliz@gmail.com>
2022-01-13 20:06:18 -0500

    component imported

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Board } from './component/Board';
 
-function App() {
+
+const gameStyles ={
+  backgroundColor: 'salmon',
+  margin: 10,
+  padding: 20
+}
+
+const App = () =>{
   return (
-    <div className="App">
+    <div style={gameStyles} className="App">
       Game
+      <Board/>
     </div>
   );
 }
