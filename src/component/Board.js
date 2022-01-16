b831c70fbdcfb03c30b3add766701a7626d19885
Gustavo <gar.santeliz@gmail.com>
2022-01-15 20:50:36 -0500

    handleClickEvent value was defined with a ternary operator

--- src/component/Board.js
+++ src/component/Board.js
@@ -3,28 +3,44 @@ import './Board.css'
 import { Square } from './Square'
 
 export const Board = () => {
-    const initialSquares = [
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-    ]
+    // const initialSquares = [
+    //     null,
+    //     null,
+    //     null,
+    //     null,
+    //     null,
+    //     null,
+    //     null,
+    //     null,
+    //     null,
+    // ]
+    //this is another way to declare an array of 9 slot defining each one as null
+    const initialSquares = Array(9).fill(null)
 
     const [square, setSquare] = useState(initialSquares)
 
-    const handleClickEvent = (i) =>{
+    const [xIsNext, setXIsNext] = useState(true) //this is the new variable to render 'X' and 'O'
+
+    const handleClickEvent = (i) => {
+        //we can not mutate directly. 
+
+        //the right way to mutate
+
+        //1. Make a copy of square state array
+        const newSquare = [...square]
+        //2. mutate the copy, setting the i-th element to 'x'
+        newSquare[i] = xIsNext ? 'X' : 'O' //in the same variable, a ternary operator is added to render 'X' or 'O'
+        //3. call the setSquare function with the mutated copy
+        setSquare(newSquare)
+        setXIsNext(!xIsNext) //to flip the boolean value
+
         alert(`square ${i} clicked`)
     }
 
     const renderSquare = (i) => {
         return (
             <Square value={square[i]}
-            onClickEvent={()=> handleClickEvent(i)} 
+                onClickEvent={() => handleClickEvent(i)}
             />
         )
     }
