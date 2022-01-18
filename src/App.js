1174fcb20e071efb6001d96c98d2f062c1f672e8
Gustavo <gar.santeliz@gmail.com>
2022-01-17 19:16:52 -0500

    final touches

--- src/App.js
+++ src/App.js
@@ -2,15 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import { Board } from './component/Board';
 
-
-// const gameStyles ={
-//   backgroundColor: 'salmon',
-//   margin: 10,
-//   padding: 20
-// }
-
 const App = () =>{
-  
+
   return (
     <div className="App">
       Tic-Tac-Toe
